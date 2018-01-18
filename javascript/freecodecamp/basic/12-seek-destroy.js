d23366ef321125cd265f0d79e0fde969e0260fcd
Ashish Patel <ashishsushilpatel@gmail.com>
2018-01-18 16:44:58 +0530

    Fix slice error

--- javascript/freecodecamp/basic/12-seek-destroy.js
+++ javascript/freecodecamp/basic/12-seek-destroy.js
@@ -1,22 +1,20 @@
 /**
  * Created by Ashish Patel
  * Copyright © 2017 ashish.me
- * ashishsushilpatel@gmail.com 
+ * ashishsushilpatel@gmail.com
  */
 
 /**
- * Problem: 
+ * Problem:
  * You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
  * Remove all elements from the initial array that are of the same value as these arguments.
  */
 
 function destroyer(arr) {
-  console.log(arguments);
-  arrgs = Array.from(arguments.slice(1));
-  console.log(arrgs);
-  return arguments[0].filter((item)=>{
-    return item != arguments[1];
-  });
+    var itemsToRemove = Array.from(arguments).slice(1);
+    return arguments[0].filter(item => {
+        return !itemsToRemove.includes(item);
+    });
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
